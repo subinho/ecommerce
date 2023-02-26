3d8681c745d9ba5af437014ff4da7116141a7894
stepan <stepansubrt@icloud.com>
2023-02-26 16:16:13 +0100

    finishing setting up sanity

---
+++ eshop/schemas/products.ts
@@ -0,0 +1,12 @@
+export default { 
+    title: 'Products',
+    name: 'products',
+    type: 'document',
+    fields: [
+        {
+            title: 'Name',
+            name: 'name',
+            type: 'string'
+        }
+    ]
+}
