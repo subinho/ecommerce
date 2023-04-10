d494ec9062894ce969d8dea22691d0492d173018
stepan <stepansubrt@icloud.com>
2023-04-10 16:48:06 +0200

    build: function for adding items to a cart

--- context/CartContext.tsx
+++ context/CartContext.tsx
@@ -1,19 +1,30 @@
-import React, { useState, createContext, useContext } from 'react'
+import React, { useState, useEffect, createContext, useContext } from 'react'
+import { ProductInfo } from '../interfaces/global_interfaces'
+
 
 type StateType = {
     qty: number;
     setQty:  React.Dispatch<React.SetStateAction<number>>;
-    increaseQty: () => void;
-    decreaseQty: () => void;
+    increaseQty(): void;
+    decreaseQty(): void;
+    cartItems: ProductInfo[];
+    setCartItems:  React.Dispatch<React.SetStateAction<ProductInfo[]>>;
+    addToCart: (product: ProductInfo, quantity: number) => void;
+    showCart: boolean;
+    setShowCart: React.Dispatch<React.SetStateAction<boolean>>; 
+    displayCart(): void;
     
-    
-}
+ }
+
 
 const Context = createContext<StateType>({} as StateType)
 
 const CartContext = ({children} : any) => {
+    let cartInfo
     const [qty, setQty] = useState(1)
-
+    const [cartItems, setCartItems] = useState<ProductInfo[]>([])
+    const [showCart, setShowCart] = useState(false)
+    // Quantity
     const increaseQty = () => {
         setQty(prev => prev + 1)
     }
@@ -22,12 +33,40 @@ const CartContext = ({children} : any) => {
         setQty(prev => prev - 1 < 1 ? 1 : prev - 1)
     }
 
+    // Cart
+    const addToCart = (product: ProductInfo, quantity: number) => {
+
+        if(!cartItems.find((item) => item._id === product._id)) {
+            setCartItems([...cartItems, {...product, quantity: quantity}])
+        } else {
+            const newCartItems =  cartItems.map((item) => {
+                if(item._id === product._id) {
+                    return  {...item, quantity: item?.quantity! + quantity}
+                } else return item
+            })
+
+            setCartItems(newCartItems)
+            // localStorage.setItem('cartItems', JSON.stringify(cartItems))
+
+        }
+        console.log(cartItems)
+    }
+
+    const displayCart = () => setShowCart(prev => !prev)
+    
+
   return (
     <Context.Provider value={{
         qty,
         setQty,
         increaseQty,
-        decreaseQty
+        decreaseQty,
+        cartItems,
+        setCartItems,
+        addToCart,
+        showCart,
+        setShowCart,
+        displayCart,
     }}>
         {children}
     </Context.Provider>
